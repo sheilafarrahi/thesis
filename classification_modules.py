e31af215135019694e0fc28d516b8b9eee0b04e4
sheila <sh.farrahi@gmail.com>
2023-02-22 16:29:21 +0100

    update-feb-22

---
+++ classification_modules.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.utils import resample
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.ensemble import GradientBoostingClassifier
+
+
+def prepare_data(df, test_size):
+    X = df.drop([0,'dist'], axis=1)
+    y = df['dist']
+    
+    # Scaling data
+    scaler = StandardScaler()
+    scaler.fit(X)
+    X_scaled = scaler.transform(X)
+
+    # train-test split
+    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size = test_size, random_state=10)
+    return X, y, X_train, X_test, y_train, y_test
+
+
+def svm_model(df, test_size, cv):
+    X, y, X_train, X_test, y_train, y_test = prepare_data(df, test_size)
+    param_grid = [
+        {'C':np.logspace(0,1,10),
+         'gamma':np.logspace(0,1,10), 
+         'kernel':['rbf']},
+    ]
+
+    optimal_params = GridSearchCV(SVC(), param_grid,cv=cv, verbose=0)
+    optimal_params.fit(X_train, y_train)
+
+    clf_svm = SVC(random_state=10, C=optimal_params.best_params_['C'], gamma=optimal_params.best_params_['gamma'])
+    clf_svm.fit(X_train, y_train)
+
+    y_pred = clf_svm.predict(X_test)
+    c_matrix = confusion_matrix(y_test, y_pred)
+
+    disp = ConfusionMatrixDisplay(c_matrix, display_labels=clf_svm.classes_)
+    disp.plot(cmap=plt.cm.Blues, colorbar=False, xticks_rotation='vertical')
+    plt.show()
+    
+    scores = cross_val_score(clf_svm, X_train, y_train, cv=cv)
+    accuracy = scores.mean()
+  
+    return accuracy, scores
--- density_estimation_modules.py
+++ density_estimation_modules.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import numpy as np
 import scipy.stats as stats
+import matplotlib.pyplot as plt
 from sklearn.neighbors import KernelDensity
+from statsmodels.distributions.empirical_distribution import ECDF
 
 ##################################
 #        methods of moments      #
 ##################################
 
 def get_moments_df(distributions_dict, nr_moments, nr_sample, sample_size):
+    # 
     m1 = list()
     x = np.zeros((nr_moments-1,nr_sample))
     df = pd.DataFrame()
@@ -32,7 +35,7 @@ def get_moments_df(distributions_dict, nr_moments, nr_sample, sample_size):
 def get_histogram_of_moments(df):
     for i in range(len(df.columns)-1):
         fig, ax = plt.subplots()
-        ax.hist(moments_df.iloc[:,i], density=True, histtype='stepfilled', bins='auto')
+        ax.hist(df.iloc[:,i], density=True, histtype='stepfilled', bins='auto')
         plt.title('moment ' + str(i))
         
 
@@ -40,7 +43,7 @@ def get_histogram_of_moments(df):
 #    Kernel density estimation   #
 ##################################
 
-def get_kde_estimates(distributions_dict, nr_sample, sample_size, x_values, bandwidth):
+def get_kde(distributions_dict, nr_sample, sample_size, x):
     df = pd.DataFrame()
     for i, (name, distr) in enumerate(distributions_dict.items()):
         y_estimates = list()
@@ -48,19 +51,65 @@ def get_kde_estimates(distributions_dict, nr_sample, sample_size, x_values, band
 
         for j in range(nr_sample):
             X = samples[j,:]
-            X = X.reshape((len(X),1))
-
-            kde = KernelDensity(kernel='gaussian', bandwidth=bandwidth).fit(X)
-
-            values = np.linspace(0,1,x_values)
-            values = values.reshape((len(values),1))
-
-            log_density = kde.score_samples(values)
-            y_estimates.append(np.exp(log_density))
+            kde = stats.gaussian_kde(X)
+            values = x
+            y_estimates.append(kde(values))
 
         df_per_dist = pd.DataFrame(y_estimates)  
         df_per_dist['dist'] = name
-
         df = pd.concat([df,df_per_dist], ignore_index=True)
 
     return df 
+
+#this works only for bounded dists
+def get_kde_plot(distributions_dict, kde_df, nr_sample, sample_size, x):
+    for name,distr in (distributions_dict.items()):
+        fig, ax = plt.subplots()
+        temp = kde_df.loc[kde_df['dist']==name]
+        for i in range(len(x)):
+            y = temp.iloc[i]
+            dist_name = y[-1:][0]
+            y = y[:-1]
+            ax.plot(x, y, c='#1f77b4', alpha=0.4)
+            ax.set_title(dist_name)
+            
+
+##################################
+#  Empirical density estimation  #
+##################################
+
+def get_edf(distributions_dict, nr_sample, sample_size, x):
+    # distribution dict: a dictinary containing different distribution including preselected parameters
+    # nr_sample: number of samples
+    # sample size: size of each sample
+    # x: array to calculate empirical cdf for it's values
+    
+    df = pd.DataFrame() # empty dataframe to store empirical CDF 
+    
+    for i, (name, distr) in enumerate(distributions_dict.items()): # iterate over each distribution
+        cum_p = list() # empty list to store cumulative probability
+        samples = distr.rvs(size=(nr_sample, sample_size), random_state=10) # get sample for each distribution
+        # iterate over each sample
+        for j in range(nr_sample):
+            ecdf = ECDF(samples[j])
+            cum_p.append(ecdf(x)) # append empirical CDF of values in x
+        
+        df_per_dist = pd.DataFrame(cum_p)
+        df_per_dist['dist'] = name
+
+        df = pd.concat([df, df_per_dist], ignore_index = True)
+    return df 
+
+
+def get_edf_plot(distributions_dict, edf_df, nr_sample, sample_size, x):
+    for name,distr in (distributions_dict.items()):# iterate over each distribution
+        fig, ax = plt.subplots()
+        temp = edf_df.loc[edf_df['dist']==name]
+
+        for i in range(nr_sample): # iterate over each sample
+            y = temp.iloc[i]
+            dist_name = y[-1:][0] 
+            y = y[:-1] # excluding  the last element which is the distribution name
+
+            ax.plot(x, y, c='#1f77b4', alpha=0.4)  
+            ax.set_title(dist_name)
