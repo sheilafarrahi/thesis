82f54ca1eb03b8176b7bab11cb3961df6e62fc2b
sheila <sh.farrahi@gmail.com>
2023-04-09 11:16:42 +0200

    updates

--- classification_modules.py
+++ classification_modules.py
@@ -21,7 +21,6 @@ importlib.reload(dem)
 
 
 def prepare_data(df, test_size):
-    #X = df.drop([0,'dist'], axis=1)
     X = df.iloc[:, :-1]
     y = df['dist']
     
@@ -38,15 +37,18 @@ def prepare_data(df, test_size):
 def svm_model(df, test_size, cv, plot=0):
     X, y, X_train, X_test, y_train, y_test = prepare_data(df, test_size)
     param_grid = [
-        {'C':np.logspace(0,1,10),
-         'gamma':np.logspace(0,1,10), 
+        {'C':np.logspace(-2,1,15),
+         'gamma':np.logspace(-2,1,15), 
          'kernel':['rbf']},
     ]
-
+    
     optimal_params = GridSearchCV(SVC(), param_grid,cv=cv, verbose=0)
     optimal_params.fit(X_train, y_train)
+    
+    cost = optimal_params.best_params_['C']
+    gamma = optimal_params.best_params_['gamma']
 
-    clf_svm = SVC(random_state=100, C=optimal_params.best_params_['C'], gamma=optimal_params.best_params_['gamma'])
+    clf_svm = SVC(random_state=100, C=cost, gamma=gamma)
     clf_svm.fit(X_train, y_train)
 
     if plot==1:
@@ -57,7 +59,7 @@ def svm_model(df, test_size, cv, plot=0):
         plt.show()
     
     scores = cross_val_score(clf_svm, X_train, y_train, cv=cv)
-    return scores
+    return scores, cost, gamma
 
 
 def rr_model(df, test_size, cv, plot=0):
@@ -75,21 +77,24 @@ def rr_model(df, test_size, cv, plot=0):
         plt.show()
     
     scores = cross_val_score(clf_rr, X_train, y_train, cv=cv)
-    return scores
+    alpha= clf_rr.alpha_
+    
+    return scores, alpha
 
 def cv_numsteps_samplesize(sample_size_list, num_steps_list, dists, nr_sample, cv_config, classifier, transform=False):
-    acc = list()
-    std = list()
+    acc, std, cost, gamma, alpha = list(), list(), list(), list(), list()
+    
     for i in tqdm(sample_size_list, desc ='% completed'):
         samples = dm.get_samples(dists, nr_sample, i, transform = transform)
-        acc_ = [] 
-        std_ = []
+        acc_, std_, cost_, gamma_, alpha_ = list(), list(), list(), list(), list()
 
         for j in num_steps_list:
             x = np.linspace(0,1,j)
             df = dem.get_kde(samples, x)
             if classifier == 1:
-                score = svm_model(df,cv_config[0], cv_config[1])
+                score, c, g = svm_model(df,cv_config[0], cv_config[1])
+                cost_.append(c)
+                gamma_.append(g)
             elif classifier == 2:
                 score = rr_model(df,cv_config[0], cv_config[1])
             acc_.append(score.mean())
@@ -97,66 +102,12 @@ def cv_numsteps_samplesize(sample_size_list, num_steps_list, dists, nr_sample, c
 
         acc.append(acc_)
         std.append(std_)
+        cost.append(cost_)
+        gamma.append(gamma_)
         sleep(0.1)
-    return acc, std
+    return acc, std, cost, gamma
 
-def cv_num_steps_step_size(step_size_list, num_steps_list, dists, sample_config, cv_config, classifier):
-    # sample_config: 
-    # cv_config: array of configuration for cross validation [test size, #splits for cross validation]
-    # classifier: integer value, 1: svm, 2: Ridge Regression
-    sample_dict = dm.get_samples(dists, sample_config[1], sample_config[0])
-    acc = list()
-    std = list()
-    
-    for i in tqdm(range(len(num_steps_list))):
-        num_t_steps = num_steps_list[i]
-        acc_ = []
-        std_ = []
-
-        for j in range(len(step_size_list)):
-            t = np.arange(1, num_t_steps+1) * step_size_list[j]
-            ecf_df = dem.get_ecf(sample_dict, t)
-            if classifier == 1:
-                score = svm_model(ecf_df,cv_config[0], cv_config[1])
-            elif classifier == 2:
-                score = rr_model(ecf_df,cv_config[0], cv_config[1])
-            acc_.append(score.mean()) 
-            std_.append(score.std()) 
-        acc.append(acc_)
-        std.append(std_)
-    return acc, std
-
-
-def cv_moments_sample_size(sample_size_list, nr_moments_list, dists, nr_sample, cv_config, classifier, transform = False):
-    # sample_size_list: list of different sample sizes to test
-    # nr_moments_list: list of different number of moments to test
-    # dists: bounded_dists or heavytail_dists
-    # nr_sample: 
-    # cv_config: array of configuration for cross validation [test size, #splits for cross validation]
-    # classifier: integer value, 1: svm, 2: Ridge Regression
-    
-    acc = list()
-    std = list()
-    for i in tqdm(sample_size_list, desc='% completed'):
-        samples = dm.get_samples(dists, nr_sample, i, transform = transform)
-        acc_ = [] 
-        std_ = []
-
-        for j in nr_moments_list:
-            df = dem.get_moments_df(samples, j)
-            if classifier == 1:
-                score = svm_model(df,cv_config[0], cv_config[1])
-            elif classifier == 2:
-                score = rr_model(df,cv_config[0], cv_config[1])
-            acc_.append(score.mean())
-            std_.append(score.std())
-
-        acc.append(acc_)
-        std.append(std_)
-        sleep(0.1)
-    return acc, std
-
-def plot_cv_moments(sample_size_list, nr_moments_list, acc, std, errbar=0):
+def plot_cv_numsteps_samplesize(sample_size_list, num_steps_list, acc, std, errbar=0):
     # sample_size_list: list of different sample sizes to test
     # nr_moments_list: list of different number of moments to test
     # acc: list of accuracy
@@ -165,12 +116,102 @@ def plot_cv_moments(sample_size_list, nr_moments_list, acc, std, errbar=0):
     fig, ax = plt.subplots(figsize=(10,8))
     for i in range(len(sample_size_list)):
         if errbar == 0 :
-            plt.plot(nr_moments_list, acc[i], label=str(nr_moments_list[i]), alpha = 0.5)
+            plt.plot(num_steps_list, acc[i], label=str(num_steps_list[i]), alpha = 0.5)
         elif errbar == 1:
-            ax.errorbar(nr_moments_list,acc[i],yerr= std[i], fmt='-', capsize=4, label=str(sample_size_list[i]), alpha=0.5)
-        ax.legend(loc='upper right', bbox_to_anchor=(1.2, 1), title='sample size')
-        plt.title('accuracy for diffrent sample size and moments')
-        plt.xlabel('moments')
-        plt.ylabel('accuracy')
+            ax.errorbar(num_steps_list,acc[i],yerr= std[i], fmt='-', capsize=4, label=str(sample_size_list[i]), alpha=0.5)
+        ax.legend(loc='upper right', bbox_to_anchor=(1.2, 1), title='Sample Size')
+        plt.title('Accuracy for Diffrent Sample Size and Number of Steps')
+        plt.xlabel('Number of Steps')
+        plt.ylabel('Accuracy')
         plt.ylim(0,1.1)
     plt.show()
+
+
+def cv_samplesize_moments(sample_size_list, nr_moments_list, dists, nr_sample, cv_config, classifier, transform = False):
+    # sample_size_list: list of different sample sizes to test
+    # nr_moments_list: list of different number of moments to test
+    # dists: bounded_dists or heavytail_dists
+    # nr_sample: 
+    # cv_config: array of configuration for cross validation [test size, #splits for cross validation]
+    # classifier: integer value, 1: svm, 2: Ridge Regression
+    acc, std, cost, gamma, alpha = list(), list(), list(), list(), list()
+    for i in tqdm(sample_size_list, desc='Completed'):
+        samples = dm.get_samples(dists, nr_sample, i, transform = transform)
+        acc_, std_, cost_, gamma_, alpha_ = list(), list(), list(), list(), list()
+
+        for j in nr_moments_list:
+            df = dem.get_moments_df(samples, j)
+            if classifier == 1:
+                score, c, g = svm_model(df,cv_config[0], cv_config[1])
+                cost_.append(c)
+                gamma_.append(g)
+            elif classifier == 2:
+                score, a = rr_model(df,cv_config[0], cv_config[1])
+                alpha_.append(a)
+            acc_.append(score.mean())
+            std_.append(score.std())
+
+        acc.append(acc_)
+        std.append(std_)
+        cost.append(cost_)
+        gamma.append(gamma_)
+        alpha.append(alpha_)
+        
+        result = dict(zip(['acc','std','cost','gamma','alpha','sample_size','nr_moments'],
+                          [acc, std, cost, gamma, alpha, sample_size_list, nr_moments_list]))
+        
+        sleep(0.1)
+    return result
+
+def plot_cv_moments(clf_result, errbar=0):
+    # clf_result is an output dict from classification that includes:
+        # sample_size: list of different sample sizes to test
+        # nr_moments: list of different number of moments to test
+        # acc: list of accuracy
+        # std: list of standard deviations
+        # cost
+        # gamma
+        # alpha
+    # errbar: 1 if error bar should be included, 0 otherwise
+    fig, ax = plt.subplots(figsize=(10,8))
+    for i in range(len(clf_result['sample_size'])):
+        if errbar == 0 :
+            plt.plot(clf_result['nr_moments'], 
+                     clf_result['acc'][i], 
+                     label=str(clf_result['sample_size'][i]), 
+                     alpha = 0.5)
+        elif errbar == 1:
+            ax.errorbar(clf_result['nr_moments'],
+                        clf_result['acc'][i],
+                        yerr= clf_result['std'][i], 
+                        fmt='-', 
+                        capsize=4, 
+                        label=str(clf_result['sample_size'][i]), 
+                        alpha=0.5)
+            
+        ax.legend(loc='upper right', bbox_to_anchor=(1.2, 1), title='Sample Size')
+        plt.title('Accuracy for Diffrent Sample Size and Moments')
+        plt.xlabel('Moments')
+        plt.ylabel('Accuracy')
+        plt.ylim(0,1.1)
+    plt.show()
+    
+
+    
+def plot_cv_h_params(clf_result):
+    if bool(clf_result['cost'][0]):
+        fig, (ax1, ax2) = plt.subplots(1,2, figsize=(15,5))
+        for i in range(len(clf_result['gamma'])):
+            ax1.plot(clf_result['nr_moments'], clf_result['gamma'][i], label = str(clf_result['sample_size'][i]))
+            ax1.set_title('Optimal Gamma')
+
+            ax2.plot(clf_result['nr_moments'], clf_result['cost'][i], label = str(clf_result['sample_size'][i]))
+            ax2.set_title('Optimal Cost')
+            ax2.legend(loc='upper right', bbox_to_anchor=(1.2, 1), title='Sample Size')
+        plt.show()
+    else:
+        fig, ax = plt.subplots(figsize=(8,5))
+        for i in range(len(clf_result['alpha'])):
+            ax.plot(clf_result['nr_moments'], clf_result['alpha'][i], label = str(clf_result['sample_size'][i]))
+            ax.set_title('Optimal Alpha')
+            ax.legend(loc='upper right', bbox_to_anchor=(1.2, 1), title='Sample Size')
